19cb02aa1c7765e5a333816dbe1178878195557c
gaddario98 <gaddario98@gmail.com>
2025-10-16 23:32:25 +0200

    update package

---
+++ rollup.dts.config.js
@@ -0,0 +1,14 @@
+import { createTypeDeclarations } from "../../rollup.common.config.js";
+
+// Definizione degli entry points (deve essere sincronizzata con rollup.config.js)
+const entries = [
+  { name: "index", input: "index.ts" },
+  { name: "index", input: "index.ts" },
+  { name: "components", input: "components/index.ts" },
+  { name: "config", input: "config/index.ts" },
+  { name: "hooks", input: "hooks/index.ts" },
+  { name: "utils", input: "utils/index.ts" },
+];
+
+// Configurazione per le dichiarazioni TypeScript
+export default createTypeDeclarations(entries);
